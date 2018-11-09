f46e23ebc05ec5ec2eac2d872a2e56dff5ce5d80
randomlysa <su@randomlysa.com>
2018-11-09 10:34:37 -0600

    add ability to login

--- client/imports/ui/Home.js
+++ client/imports/ui/Home.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Meteor } from 'meteor/meteor'
 
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ''
+    }
+  } // constructor
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({email}, password, (e) => {
+      console.log('login callback', e)
+    })
+  } // onSubmit
+
   render() {
     return (
       <div>
         <h1>Login to Short Lnk</h1>
 
-        <p><em>login form here</em></p>
+        {this.state.error ? <p>{this.state.error}</p> : ''}
+
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name="email" placeholder="Email" />
+          <input type="password" ref="password" name="password" placeholder="Password" />
+          <button>Login</button>
+        </form>
 
         <Link to="/signup">Sign up</Link>
       </div>

b5b56f640d331df9f23e41fef291cf0914a19d05
randomlysa <su@randomlysa.com>
2018-11-09 14:45:08 -0600

    some cleanup

--- client/main.js
+++ client/main.js
@@ -1,15 +1,13 @@
 import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
-import { Tracker } from 'meteor/tracker'
 
-// import AppRouter from './imports/ui/AppRouter'
 import { routes, onAuthChange } from '../imports/routes/routes.js'
 
 Tracker.autorun(() => {
   let isAuthenticated = !!Meteor.userId()
   onAuthChange(isAuthenticated)
-})
+});
 
 Meteor.startup(() => {
   ReactDOM.render(routes, document.getElementById('app'))
-})
+});

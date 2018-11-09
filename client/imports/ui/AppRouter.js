46666088fac1579610c968faa9d55bb5f80b9cf7
randomlysa <su@randomlysa.com>
2018-11-08 22:20:38 -0600

    set up router

--- client/imports/ui/AppRouter.js
+++ client/imports/ui/AppRouter.js
@@ -1,12 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+import Home from './Home';
 import Signup from './Signup';
-// import Link from './Link';
+import Links from './Links';
+import NotFound from './NotFound';
 
 const AppRouter = () => (
   <Router>
-      <Route path="/signup/" component={Signup} />
+    <div>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signup/" component={Signup} />
+        <Route path="/links/" component={Links} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </Router>
 );
 

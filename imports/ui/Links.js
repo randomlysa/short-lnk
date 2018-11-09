34898b7eb84b39da8d80c24f9671f80d1b10904f
randomlysa <su@randomlysa.com>
2018-11-09 17:29:59 -0600

    use Meteor method to insert url

--- imports/ui/Links.js
+++ imports/ui/Links.js
@@ -21,7 +21,7 @@ export default class Link extends React.Component {
     const url = this.refs.url.value.trim();
 
     if(url) {
-      Links.insert({ url, userId: Meteor.userId() });
+      Meteor.call('links.insert', url)
       this.refs.url.value = '';
     }
   }

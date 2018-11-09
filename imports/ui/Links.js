fa88edfd11d0a098f21f5e08d2177a8ae765c380
randomlysa <su@randomlysa.com>
2018-11-09 14:24:54 -0600

    basic form to submit links

--- imports/ui/Links.js
+++ imports/ui/Links.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base'
 import onEnterPrivatePage from '../utils/onEnterPrivatePage';
 
+import { Links } from '../api/links'
 
 export default class Link extends React.Component {
   doLogout() {
@@ -12,11 +13,30 @@ export default class Link extends React.Component {
     onEnterPrivatePage()
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+
+    const url = this.refs.url.value.trim();
+
+    if(url) {
+      Links.insert({ url });
+      this.refs.url.value = '';
+    }
+  }
+
   render() {
     return (
       <div>
         <h1>Links</h1>
         <button onClick={this.doLogout.bind(this)}>Logout</button>
+
+        <hr />
+        <h3>Add Link</h3>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="Your URL" />
+          <button>Add Link</button>
+        </form>
+
       </div>
     )
   }

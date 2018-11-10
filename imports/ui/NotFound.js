b92841444265aae02c3eafcd0b7c95d3b6285d4c
randomlysa <su@randomlysa.com>
2018-11-09 20:24:06 -0600

    convert to stateless functional component

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class NotFound extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Not Found</h1>
-        <Link to="/">Go Home</Link>
-      </div>
-    )
-  }
+export default () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <Link to="/">Go Home</Link>
+    </div>
+  )
 }
--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -2,19 +2,14 @@ import React from 'react';
 import { Accounts } from 'meteor/accounts-base'
 import PropTypes from 'prop-types'
 
-export default class PrivateHeader extends React.Component {
-  doLogout() {
-    Accounts.logout()
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <button onClick={this.doLogout.bind(this)}>Logout</button>
-      </div>
-    )
-  } // render
-} // export default class PrivateHeader
+export default PrivateHeader = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <button onClick={ ()=> Accounts.logout() }>Logout</button>
+    </div>
+  )
+} // export default PrivateHeader
 
 PrivateHeader.propTypes = {
   title: PropTypes.string.isRequired

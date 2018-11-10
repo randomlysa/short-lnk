85aab5343449402d833782e0e64175c20953bfc0
randomlysa <su@randomlysa.com>
2018-11-09 18:48:30 -0600

    change how SimpleSchema shows an error
    no longer requires try/catch

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor';
 import ReactDOM from 'react-dom';
 
 import { routes, onAuthChange } from '../imports/routes/routes.js'
+import '../imports/startup/simpleSchemaConf';
 
 Tracker.autorun(() => {
   let isAuthenticated = !!Meteor.userId()

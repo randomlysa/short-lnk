fabb9f0e1427112567316fde9d3ee1c94b3e5e36
randomlysa <su@randomlysa.com>
2018-11-09 19:18:46 -0600

    redirect if link is found in database

--- server/main.js
+++ server/main.js
@@ -1,9 +1,24 @@
 import { Meteor } from 'meteor/meteor';
+import { WebApp } from 'meteor/webapp';
 
 import '../imports/api/users'
-import '../imports/api/links'
+import { Links } from '../imports/api/links'
+import '../imports/startup/simpleSchemaConf';
 
 Meteor.startup(() => {
+  WebApp.connectHandlers.use((req, res, next) => {
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id });
 
-
+    if (link) {
+      // Set HTTP status code.
+      res.statusCode = 302;
+      // Set HTTP headers.
+      res.setHeader('Location', link.url);
+      // End HTTP request
+      res.end();
+    } else {
+      next();
+    }
+  })
 });

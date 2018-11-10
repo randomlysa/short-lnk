99fdd5a0982960f5440fab1fd33c1b5eb02440bb
randomlysa <su@randomlysa.com>
2018-11-10 14:08:31 -0600

    use state to manage url

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -1,26 +1,40 @@
 import React from 'react';
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { url: '' }
+  }
+  onChange(e) {
+    this.setState({ url: e.target.value })
+  }
+
   onSubmit(e) {
     e.preventDefault();
-
-    const url = this.refs.url.value.trim();
+    const { url } = this.state;
 
     if(url) {
-      Meteor.call('links.insert', url)
-      this.refs.url.value = '';
-    }
-  }
+      Meteor.call('links.insert', url, (err, res) => {
+        // If there's no error, remove the url.
+        if(!err) this.setState({ url: '' })
+      }) // Meteor.call
+    } // if(url)
+  } // onSubmit
 
   render() {
     return (
       <div>
         <h3>Add Link</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="Your URL" />
+          <input
+            type="text"
+            placeholder="Your URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}
+          />
           <button>Add Link</button>
         </form>
       </div>
-    )
+    ) // return
   } // render
 } // export default class AddLink

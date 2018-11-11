62b02af7be8fce39449b8a716be7a14fbbbec6a3
randomlysa <su@randomlysa.com>
2018-11-10 20:43:01 -0600

    Sync checkbox with Session 'showVisible'

--- imports/ui/LinksListFilters.js
+++ imports/ui/LinksListFilters.js
@@ -1,15 +1,41 @@
 import React from 'react';
 import { Session } from 'meteor/session';
 
-export default () => {
-  return (
-    <div>
-      <label>
-        Show hidden links &nbsp;
-        <input type="checkbox" onChange={(e) =>{
-          visible: Session.set('showVisible', !e.target.checked)
-        }} />
-      </label>
-    </div>
-  )
-} // export default
+export default class ListLinksFilter extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showVisible: true
+    };
+  } // constructor
+
+  componentDidMount() {
+    this.checkboxTracker = Tracker.autorun(() => {
+      // Sync checkbox with Session 'showVisible'
+      this.setState({
+        showVisible: Session.get('showVisible')
+      });
+    }); // Tracker.autorun
+  } // componentDidMount
+
+  componentWillUnmount() {
+    this.checkboxTracker.stop();
+  }
+
+  render() {
+    return (
+      <div>
+        <label>
+          Show hidden links &nbsp;
+          <input
+            type="checkbox"
+            checked={!this.state.showVisible}
+            onChange={(e) =>{
+              visible: Session.set('showVisible', !e.target.checked)
+            }}
+          />
+        </label>
+      </div>
+    ) // return
+  } // render
+} // export default class

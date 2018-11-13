0823b9cba5dec8736a6e8c7e5dfba9ac330042da
randomlysa <su@randomlysa.com>
2018-11-13 13:11:41 -0600

    display errors, success, change style
    style like login/sign up

--- imports/ui/ResetPassword.js
+++ imports/ui/ResetPassword.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import PrivateHeader from './PrivateHeader'
 import { Accounts } from 'meteor/accounts-base';
+import { Link } from 'react-router-dom';
 
 export default class PasswordReset extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: ''
+      error: '',
+      success: false
     }
 
     this.token = window.location.pathname.split('/')[2];
@@ -14,38 +15,60 @@ export default class PasswordReset extends React.Component {
 
   onSubmit(e) {
     e.preventDefault();
-    // Check that passwords match and are > 7 charactrs.
+    // Check that passwords match and are > 7 characters.
     if(this.refs.newPwd.value === this.refs.newPwdConfirm.value &&
       this.refs.newPwd.value.length > 7)
     {
-      // Valid
+    // Valid passwords entered.
       this.setState({ 'error': ''});
-      Accounts.resetPassword(this.token, this.refs.newPwd.value, function(err) {
-        if (err) {
-          console.log('We are sorry but something went wrong.');
-        } else {
-          console.log('Your password has been changed. Welcome back!');
-        }
+      // Reset the password, set this.state.success: true.
+      Accounts.resetPassword(this.token, this.refs.newPwd.value, (e) => {
+        if (e)  this.setState({ error: 'Sorry, something went wrong.' });
+        else this.setState({ error: '', success: true })
       });
+    // Invalid passwords entered.
     } else {
-      // Invalid
-      this.setState({ 'error': 'Please enter matching passwords of 8 or more characters'})
-    }
-  }
+      this.setState({
+        'error': 'Please enter matching passwords of 8 or more characters'
+      });
+    } // else
+  } // onSubmit(e)
+
   render() {
+    if(this.state.success) {
+      return (
+        <div className="boxed-view">
+          <div className="boxed-view__box">
+            <h2>Success</h2>
+            Your password was reset and you have been logged in.<br />
+            <Link to="/links">Go to your links.</Link>
+          </div>
+        </div>
+      ) // return
+    } // this.state.success
+
     // https://gist.github.com/LeCoupa/9879066
     return (
-      <div>
-        <PrivateHeader title="Reset Password" />
-          <div className="page-content">
+      <div className="boxed-view">
+        <div className="boxed-view__box">
+          <h2>Reset Password</h2>
           {this.state.error ? <p>{this.state.error}</p> : undefined}
-            <form onSubmit={this.onSubmit.bind(this)}>
-              <input ref="newPwd" name="password" placeholder="New Password" type="password" />
-              <input ref="newPwdConfirm" name="password-confirm" placeholder="Confirm" type="password" />
-
+          <form onSubmit={this.onSubmit.bind(this)}>
+            <input
+              ref="newPwd"
+              name="password"
+              placeholder="New Password"
+              type="password"
+            />
+            <input
+              ref="newPwdConfirm"
+              name="password-confirm"
+              placeholder="Confirm"
+              type="password"
+            />
             <button className="button">Reset Password</button>
-            </form>
-          </div>
+          </form>
+        </div>
       </div>
     ) // return
   } // render

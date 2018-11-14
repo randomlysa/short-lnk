65faaeb2453c1fcccbdfcc279846baa4ce8eb0f2
randomlysa <su@randomlysa.com>
2018-11-14 14:56:06 -0600

    test error for short password

--- imports/ui/Signup.test.js
+++ imports/ui/Signup.test.js
@@ -46,8 +46,22 @@ if (Meteor.isClient) {
       expect(spy).to.have.been.called.with({ email, password} );
     });
 
-    it('should set loginWithPassword errors', function() {
+    it('should set error when password is short', function() {
+      const email = 'test@example.com';
+      const password = 'pass16';
+      const spy = chai.spy();
+      const wrapper = mount (
+        <MemoryRouter>
+          <Signup createUser={spy} />
+        </MemoryRouter>
+      );
 
+      wrapper.find('input').at(0).instance().value = email;
+      wrapper.find('input').at(1).instance().value = password;
+      wrapper.find('form').simulate('submit');
+
+      expect(wrapper.find(Signup).instance().state.error).to
+        .equal('Password must be at least 8 characters long');
     });
 
   }) // describe

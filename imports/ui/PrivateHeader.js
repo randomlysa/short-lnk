8db6e36dd9af5ba0a166c24e24cd7e4f398e0191
randomlysa <su@randomlysa.com>
2018-11-13 20:39:53 -0600

    test PrivateHeader button click runs a function

--- imports/ui/PrivateHeader.js
+++ imports/ui/PrivateHeader.js
@@ -7,12 +7,13 @@ export default PrivateHeader = (props) => {
     <div className="header">
       <div className="header__content">
         <h1 className="header__title">{props.title}</h1>
-        <button className="button button--link-text" onClick={ ()=> Accounts.logout() }>Logout</button>
+        <button className="button button--link-text" onClick={ ()=> props.handleLogout() }>Logout</button>
       </div>
     </div>
   )
 } // export default PrivateHeader
 
 PrivateHeader.propTypes = {
-  title: PropTypes.string.isRequired
+  title: PropTypes.string.isRequired,
+  handleLogout: PropTypes.func.isRequired
 }
---
+++ imports/ui/PrivateHeader.test.js
@@ -0,0 +1,27 @@
+import { Meteor } from 'meteor/meteor';
+import React from 'react';
+import chai, { expect } from 'chai';
+import spies from 'chai-spies';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import PrivateHeader from './PrivateHeader';
+
+// Setup
+configure({ adapter: new Adapter() });
+chai.use(spies);
+
+// Tests
+if (Meteor.isClient) {
+  describe('PrivateHeader', function() {
+    it('should call handleLogout on click', function() {
+      const spy = chai.spy();
+      const wrapper = mount (
+        <PrivateHeader title="Test" handleLogout={spy} />
+      );
+
+      wrapper.find('button').simulate('click')
+      expect(spy).to.have.been.called()
+    })
+  });
+}
